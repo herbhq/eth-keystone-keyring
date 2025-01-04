25a0a4b20d71198f1ee1e00a46d4815505f81555
Sahil Arun <151955025+sahilarun@users.noreply.github.com>
2025-01-04 17:04:38 +0530

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import { MetaMaskKeyring } from "@keystonehq/metamask-airgapped-keyring";
+import { toChecksumAddress } from 'ethereumjs-util';
+
+const pathBase = 'm';
+
+export default class KeystoneKeyring extends MetaMaskKeyring {
+  perPage = 10;
+  
+  async getAddresses(start: number, end: number) {
+    if (!this.initialized) {
+      await this.readKeyring();
+    }
+    const accounts = [];
+    for (let i = start; i < end; i++) {
+      const address = await this.__addressFromIndex(pathBase, i);
+      accounts.push({
+        address,
+        balance: null,
+        index: i,
+      });
+      this.indexes[toChecksumAddress(address)] = i;
+    }
+    return accounts;
+  }
+}
